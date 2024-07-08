f8dc2a63f8aa6b894640ca4a65c3a2d46f70c32f
ardhendusgit <crackednutxx@gmail.com>
2024-07-08 18:53:19 +0530

    check returns

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,10 +5,10 @@ module "vpc" {
   name = "vprofile-eks"
 
   cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
 
   #private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
+  public_subnets  = ["172.20.1.0/24", "172.20.2.0/24"]
 
   enable_nat_gateway   = false
   enable_dns_hostnames = true
